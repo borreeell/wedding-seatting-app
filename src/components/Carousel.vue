ba51b7d3138ccb7efa4939653c4bbd16b762576c
ViperBite03 <layitamartin@gmail.com>
2025-07-14 20:42:32 +0200

    better code setup

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,45 +1,62 @@
 <template>
   <div class="layout-container">
-    <h1 class="title">Layout {{layoutNum}}</h1>
+    <h1 class="title">Layout {{ layoutNum }}</h1>
 
-    <div class="carousel-container" >
+    <div class="carousel-container">
       <button @click="restLayout">
-        <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path  d="M6.325 12.85q-.225-.15-.337-.375T5.874 12t.113-.475t.337-.375l8.15-5.175q.125-.075.263-.112T15 5.825q.4 0 .7.288t.3.712v10.35q0 .425-.3.713t-.7.287q-.125 0-.262-.038t-.263-.112z"/></svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="128"
+          height="128"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M6.325 12.85q-.225-.15-.337-.375T5.874 12t.113-.475t.337-.375l8.15-5.175q.125-.075.263-.112T15 5.825q.4 0 .7.288t.3.712v10.35q0 .425-.3.713t-.7.287q-.125 0-.262-.038t-.263-.112z"
+          />
+        </svg>
       </button>
-      
-      <img :src="`/layout${layoutNum}.jpg`" :alt="`Layout ${layoutNum}`">
+
+      <img :src="`/layout${layoutNum}.jpg`" :alt="`Layout ${layoutNum}`" />
 
       <SeatButton></SeatButton>
 
       <button @click="sumLayout">
-        <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path d="M8 17.175V6.825q0-.425.3-.713t.7-.287q.125 0 .263.037t.262.113l8.15 5.175q.225.15.338.375t.112.475t-.112.475t-.338.375l-8.15 5.175q-.125.075-.262.113T9 18.175q-.4 0-.7-.288t-.3-.712"/></svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="128"
+          height="128"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M8 17.175V6.825q0-.425.3-.713t.7-.287q.125 0 .263.037t.262.113l8.15 5.175q.225.15.338.375t.112.475t-.112.475t-.338.375l-8.15 5.175q-.125.075-.262.113T9 18.175q-.4 0-.7-.288t-.3-.712"
+          />
+        </svg>
       </button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import SeatButton from './resources/SeatButton.vue'
+import { ref } from "vue";
+import SeatButton from "./resources/SeatButton.vue";
 
-let layoutNum = ref(1)
+let layoutNum = ref(1);
 
 const sumLayout = () => {
-  layoutNum.value = layoutNum.value < 4 ? layoutNum.value + 1 : 1
-}
+  layoutNum.value = layoutNum.value < 4 ? layoutNum.value + 1 : 1;
+};
 
 const restLayout = () => {
-  layoutNum.value = layoutNum.value == 1 ? 4 : layoutNum.value - 1
-}
+  layoutNum.value = layoutNum.value == 1 ? 4 : layoutNum.value - 1;
+};
 
 const layouts = [
-  { src: '/layout1.jpg', alt: 'Layout 1' },
-  { src: '/layout2.jpg', alt: 'Layout 2' },
-  { src: '/layout3.jpg', alt: 'Layout 3' },
-  { src: '/layout4.jpg', alt: 'Layout 4' },
-]
+  { src: "/layout1.jpg", alt: "Layout 1" },
+  { src: "/layout2.jpg", alt: "Layout 2" },
+  { src: "/layout3.jpg", alt: "Layout 3" },
+  { src: "/layout4.jpg", alt: "Layout 4" },
+];
 </script>
-
 
 <style scoped>
 .layout-container {
@@ -49,7 +66,7 @@ const layouts = [
   padding: 2rem;
   text-align: center;
 
-position: relative;
+  position: relative;
 }
 
 .carousel-container {
